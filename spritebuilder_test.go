35c475bfbab2c2701d8ab058a60b8f2f0e6a6413
kyokomi <kyoko1220adword@gmail.com>
2014-08-20 09:35:54 +0900

    test: refactor test

--- spritebuilder_test.go
+++ spritebuilder_test.go
@@ -5,8 +5,20 @@ import (
 )
 
 const (
+	testCCBDirPath = "test/Example.spritebuilder/SpriteBuilder Resources/"
 	testCCBFilePath = "test/Example.spritebuilder/SpriteBuilder Resources/MainScene.ccb"
 )
+
+func TestCheckReadCCBFile(t *testing.T) {
+	if err := CheckReadCCBFile(testCCBFilePath); err != nil {
+		t.Error(err)
+	}
+}
+func TestCheckReadCCBDir(t *testing.T) {
+	if err := CheckReadCCBDir(testCCBDirPath); err != nil {
+		t.Error(err)
+	}
+}
 
 func TestDecodeFileJSON(t *testing.T) {
 	if _, err := decodeFileJSON(testCCBFilePath); err != nil {
